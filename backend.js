2a95d1440fd792628167f3bbe6d96e7c60e95e64
Pradip Sarkar <sarkar.pradip2002@gmailcom>
2021-08-13 10:13:43 +0530

    Initial Commit

---
+++ backend.js
@@ -0,0 +1,45 @@
+let express=require('express');
+let fs=require('fs');
+let path=require('path');
+// const bodyparser=require('body parser');
+const mongoose=require('mongoose');
+mongoose.connect('mongodb://localhost/singingcontact', {useNewUrlParser: true, useUnifiedTopology: true});
+let app=express();
+const port=5000;
+
+var db=mongoose.connection;
+
+var contactschema=new mongoose.Schema({
+    name: String,
+    age: String,
+    email: String,
+    mobileno: String
+});
+
+var contact=mongoose.model('contactus',contactschema);
+
+app.use('/static',express.static('static'));
+app.use(express.urlencoded());
+
+app.set('view engine','pug');
+
+app.set('views',path.join(__dirname,'views'));
+
+app.get('/',(req,res)=>{
+    res.render('home.pug');
+})
+app.get('/contact',(req,res)=>{
+    res.render('contact.pug');
+})
+app.post('/contact',(req,res)=>{
+    var mydata=new contact(req.body);
+    mydata.save().then(()=>{
+        res.render("Your data ahas been stored in database");
+    }).catch(()=>{
+        res.status(400).send("Your data has not been stored");
+    })
+})
+
+app.listen(port,()=>{
+    console.log(`The port is start at ${port}`);
+})
